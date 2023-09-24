dfceb30d79d447a2919d17552715382ea97ae48e
MateusRSdev <mateusRSdev@gmail.com>
2023-09-24 18:17:20 -0300

    acredito que agora vai

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ainda sem nome</title>
-    @vite(['../resources/sass/app.scss','../resources/js/app.js'])
+    @vite(['resources/sass/app.scss','resources/js/app.js'])
 </head>
 <body>
     <h1 class="display-3">Olá Mundo</h1>
