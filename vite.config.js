a88180b62fb89d51fbdee2a90ea55223d5e3cdcd
MateusRSdev <mateusRSdev@gmail.com>
2023-09-24 19:39:22 -0300

    acredito que agora vai

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,42 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+            'resources/sass/app.scss',
+            'resources/js/app.js',
+        ],refresh: true
+        })
     ],
+
+    // css: {
+    //     postcss: null,
+    //     preprocessorOptions: {
+    //         scss: {
+    //             additionalData: `
+    //                 @import "./resources/scss/variables";
+    //                 @import "./resources/scss/color";
+    //             `,
+    //         },
+    //     },
+    // },
+
+    resolve:{
+        alias:{
+            "@":"/resources/js",
+        }
+    },
+    server: {
+        https: false, // Use HTTPS apenas em produção
+        host: true, // Use diferentes hosts para dev e prod
+        strictPort: true,
+        port: 3009, // Use diferentes portas para dev e prod
+        hmr: { host: 'localhost', protocol: 'ws'},
+        watch: {
+            usePolling: true,
+        }
+    }
 });
