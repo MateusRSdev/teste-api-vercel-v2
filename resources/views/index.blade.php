3c0ef759b14907e02fe845c34818a53d8cac03af
MateusRSdev <mateusRSdev@gmail.com>
2023-09-24 19:56:54 -0300

    acredito que agora vai

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ainda sem nome</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    @vite(['resources/sass/app.scss','resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss','resources/js/app.js']) --}}
 </head>
 <body>
     <h1 class="display-3">Olá Mundo</h1>
