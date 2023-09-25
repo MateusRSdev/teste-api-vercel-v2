2f32c9e8882098df74e28a4fad8033aa92b08de2
MateusRSdev <mateusRSdev@gmail.com>
2023-09-25 08:22:59 -0300

    desisti do vite HHAAAAAA

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ainda sem nome</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    {{-- @vite(['resources/sass/app.scss','resources/js/app.js']) --}}
+    <link rel="stylesheet" href="{{ asset('/assets/css/app.css') }}">
 </head>
 <body>
     <h1 class="display-3">Olá Mundo</h1>
