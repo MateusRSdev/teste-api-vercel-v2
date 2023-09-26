e4e7b5d7632732f3bf38e6698cc2583033707d6b
MateusRSdev <mateusRSdev@gmail.com>
2023-09-26 07:59:52 -0300

    !

--- api/assets.php
+++ api/assets.php
@@ -6,11 +6,11 @@
  */
 $file = $_GET['assets'];
 if (str_ends_with($file, '.css')) {
-    header("Content-type: text/css; charset: UTF-8");
+    header("Content-type: text/css; charset: UTF-8; Access-Control-Allow-Origin: *");
     return require '/var/task/user/public/build/assets/' . basename($file);
     // echo require __DIR__ . '/../assets/' . basename($file);
 } else if (str_ends_with($file, '.js')) {
-    header('Content-Type: application/javascript; charset: UTF-8');
+    header('Content-Type: application/javascript; charset: UTF-8; Access-Control-Allow-Origin: *');
     return require '/var/task/user/public/build/assets/' . basename($file);
     // echo require __DIR__ . '/../assets/' . basename($file);
 }
