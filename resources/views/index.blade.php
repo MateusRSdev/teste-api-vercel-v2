b7170aaf03e174c8e82e77496646c209ae065e34
MateusRSdev <mateusRSdev@gmail.com>
2023-09-26 07:19:50 -0300

    !

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ainda sem nome</title>
-    <link rel="stylesheet" href="{{ asset('/assets/css/app.css') }}">
+   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <h1 class="display-3">Olá Mundo</h1>
