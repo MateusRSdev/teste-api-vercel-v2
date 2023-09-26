90b045f9824a8f25b4364e022b5b035df51a9282
MateusRSdev <mateusRSdev@gmail.com>
2023-09-26 06:16:00 -0300

    !

--- api/index.php
+++ api/index.php
@@ -1,16 +1,30 @@
 <?php
- 
 // Load the Laravel application
 
 // require __DIR__ . '/../vendor/autoload.php';
 // $app = require_once __DIR__ . '/../bootstrap/app.php';
- 
+
 // // Run the application
 // $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 // $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
-// $response->send();
-// $kernel->terminate($request, $response);
-require __DIR__ . '/../public/index.php';
-
+    //     $request = Illuminate\Http\Request::capture()
+    // );
+    // $response->send();
+    // $kernel->terminate($request, $response);
+    // require __DIR__ . '/../public/index.php';
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>hAAAAAA</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
+    </body>
+    </html>
+    
+    
+    
