04a8e865b70fa6831920b771cc180d8a484d3af9
MateusRSdev <mateusRSdev@gmail.com>
2023-09-26 06:39:43 -0300

    !

--- api/index.php
+++ api/index.php
@@ -11,20 +11,8 @@
     // );
     // $response->send();
     // $kernel->terminate($request, $response);
-    // require __DIR__ . '/../public/index.php';
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>hAAAAAA</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        
-    </body>
-    </html>
-    
+    require __DIR__ . '/../public/index.php';
+
+
     
     
