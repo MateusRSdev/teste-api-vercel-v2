8d5086fa701fb632acfb53730a5ef2303492fef4
MateusRSdev <mateusRSdev@gmail.com>
2023-09-27 07:55:38 -0300

    so pecçço que funcione

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ainda sem nome</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        @vite("resources/js/app.js")
+    @vite("resources/sass/app.scss")
+    
 </head>
 <body>
     <h1 class="display-3">Olá Mundo</h1>
